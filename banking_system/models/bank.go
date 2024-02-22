da9d2fcfb579971ebcca3455760556f8cbe8875a
Pranav-Busy <158021951+Pranav-Busy@users.noreply.github.com>
2024-02-22 10:49:08 +0530

    Delete banking_system directory

--- banking_system/models/bank.go
+++
@@ -1,8 +0,0 @@
-package models
-
-
-type Bank struct {
-    ID       uint               `json:"id" pg:",pk"`
-    Name     string             `json:"name" pg:",notnull"`
-    Branches []*Branch   `json:"branches" pg:"rel:has-many"`
-}
--- banking_system/models/openacc.go
+++
@@ -1,7 +0,0 @@
-package models
-type Openacc struct{
-	Branch_id   uint    `json:"branch_id" pg:",fk"`        // foreign key referencing Branch.ID
-    Balance    float64 `json:"balance" pg:",notnull"`
-    Acc_type   string  `json:"account_type" pg:",notnull"`
-	Customer []Customer `json:"customer"`
-}
